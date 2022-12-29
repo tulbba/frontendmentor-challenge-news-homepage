b15c05912d37ab70516975149f4cba1b725a16aa
Tuuli <tuuli.torkkeli@gmail.com>
2022-12-29 12:12:31 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+    }
+  });
+});
+
+$(".hamburger-button").click(function () {
+  $("body").toggleClass("nav-open");
+  $("body").toggleClass("show-overlay");
+});
